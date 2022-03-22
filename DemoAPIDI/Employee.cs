5dc217c9ca5adfb0e58c53aea5c80e582d7e3c67
Praveen Nayak <pnayak@hamiltongroupuk.com>
2022-03-22 15:21:13 +0000

    Add project files.

---
+++ DemoAPIDI/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace DemoAPIDI
+{
+    public interface IEmployee
+    {
+        public string? Name { get; set; }
+        public string Description { get; set; }
+
+    }
+    public class Employee : IEmployee
+    {
+        public string? Name { get ; set ; }
+        public string Description { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
